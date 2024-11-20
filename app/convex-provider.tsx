e9026c6d7163d8600c9d73bc6831584c499e0e5d
Sahag Arzoumanian <arzsahag@gmail.com>
2024-11-20 11:36:37 -0800

    Add custom clerk localization

--- app/convex-provider.tsx
+++ app/convex-provider.tsx
@@ -8,8 +8,17 @@ import { ReactNode } from "react";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
+  const customLocalization = {
+    signIn: {
+      start: {
+        title: "Sign in to the New Life Staff Portal", // Change the title
+        subtitle: "Welcome back! Please sign in to continue!",
+      },
+    },
+  };
   return (
     <ClerkProvider
+      localization={customLocalization}
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
     >
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,14 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex flex-1 items-center justify-center">
-      <SignIn signUpUrl="/sign-up" />
+      <SignIn
+        appearance={{
+          elements: {
+            headerTitle: "Sign in to the", // Customize header text
+          },
+        }}
+        signUpUrl="/sign-up"
+      />
     </div>
   );
 }
